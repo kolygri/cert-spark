a7231034200aaf86f6497618258404e5b5ca392d
Konstantin Grigorov <k.l.grigorov@gmail.com>
2026-07-23 01:48:42 +0300

    fix: support Vercel and Pages deployments

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/cert-spark/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github-pages' ? '/cert-spark/' : '/',
   plugins: [react()],
-})
+}))
